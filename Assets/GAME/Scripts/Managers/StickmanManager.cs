6b8290ba42d1aef1c5ee987998175084076c3bc7
bekiralci <bekiralci@outlook.com>
2023-03-07 19:54:56 +0300

    Added; Script : StickmanManager

---
+++ Assets/GAME/Scripts/Managers/StickmanManager.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StickmanManager : MonoBehaviour
+{
+    public Dictionary<int, List<StickmanController>> currentCars = new();
+
+    [SerializeField] private List<StickmanController> stickmanList_level_1;
+    [SerializeField] private List<StickmanController> stickmanList_level_2;
+    [SerializeField] private List<StickmanController> stickmanList_level_3;
+    [SerializeField] private List<StickmanController> stickmanList_level_4;
+    [SerializeField] private List<StickmanController> stickmanList_level_5;
+    [SerializeField] private List<StickmanController> stickmanList_level_6;
+
+    private void Awake()
+    {
+
+        currentCars.Add(0, stickmanList_level_1);
+        currentCars.Add(1, stickmanList_level_2);
+        currentCars.Add(2, stickmanList_level_3);
+        currentCars.Add(3, stickmanList_level_4);
+        currentCars.Add(4, stickmanList_level_5);
+        currentCars.Add(5, stickmanList_level_6);
+
+    }
+
+    #region EventManager
+
+    private void OnEnable()
+    {
+        EventManager.E_StickmansManager += GetThis;
+    }
+
+    private void OnDisable()
+    {
+        EventManager.E_StickmansManager -= GetThis;
+    }
+
+    #endregion
+
+    private StickmanManager GetThis()
+    {
+        return this;
+    }
+
+    public void AddObj(StickmanController stickman)
+    {
+
+        currentCars[stickman.level - 1].Add(stickman);
+
+    }
+    
+    public void RemoveObj(StickmanController stickman)
+    {
+
+        currentCars[stickman.level - 1].Remove(stickman);
+
+    }
+
+}
