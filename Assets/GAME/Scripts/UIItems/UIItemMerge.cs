681dafb8136da8c1c3d424e078f44fbc0772f915
bekiralci <bekiralci@outlook.com>
2023-03-08 03:28:19 +0300

    Edit; Script : UIItemMerge

--- Assets/GAME/Scripts/UIItems/UIItemMerge.cs
+++ Assets/GAME/Scripts/UIItems/UIItemMerge.cs
@@ -4,28 +4,40 @@ using UnityEngine;
 
 public class UIItemMerge : ButtonBase
 {
-    
+
+    [SerializeField] private StickmanPlacer stickmanPlacer;
+
     private bool Merge()
     {
 
         Dictionary<int, List<StickmanController>> stickmansOnGame = EventManager.E_StickmansManager.Invoke().stickmansOnGame;
 
-        for (int level = 0; level < 3; level++)
+        for (int level = 0; level < 5; level++)
         {
 
-            if (stickmansOnGame[level].Count >= 3)
+            if (stickmansOnGame[level].Count >= 3 && EventManager.E_StickmansManager.Invoke().stickmansOnGame[level + 1].Count != stickmanPlacer.maxStickmanCount)
             {
 
-                for (int j = 3 - 1; j >= 0; j--)
+                int index = stickmansOnGame[level].Count - 1;
+
+                for (int stickmanIndex = index; stickmanIndex >= index - 2; stickmanIndex--)
                 {
 
-                    stickmansOnGame[level][j].gameObject.SetActive(false);
-                    stickmansOnGame[level].Remove(stickmansOnGame[level][j]);
+                    List<StickmanController> controllers = stickmansOnGame[level];
+
+                    controllers[stickmanIndex].gameObject.SetActive(false);
+                    EventManager.E_ObjectPool.Invoke().pools[level].pooledObjects.Add(controllers[stickmanIndex]);
+                    controllers.Remove(controllers[stickmanIndex]);
+
+                    print(stickmanIndex);
 
                 }
 
                 EventManager.E_StickmansManager.Invoke().stickmansOnGame = stickmansOnGame;
-                EventManager.E_ObjectPool?.Invoke().GetPooledObject(level + 1);
+
+                StickmanController newobj = EventManager.E_ObjectPool?.Invoke().GetPooledObject(level + 1);
+
+                stickmanPlacer.SetTransform(newobj.obj_Main, newobj.level);
 
                 return true;
 
@@ -57,7 +69,7 @@ public class UIItemMerge : ButtonBase
 
     public override void OnClickButton()
     {
-        if (CheckAmount() && Merge())
+        if (/*CheckAmount() && */Merge())
         {
             UpdateCost();
             UpdateText();
